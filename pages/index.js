4e830ec096adcfab2a58a2416f9fc9798c05df81
Denis Pankov <idenispankov@gmail.com>
2021-01-31 16:16:31 -0800

    responsive at 1024, 768, 375

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-100px";
+    document.getElementById("navbar").style.top = "-200px";
   }
   prevScrollpos = currentScrollPos;
 }
