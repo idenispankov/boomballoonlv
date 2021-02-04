5bf0dbf350293f7b7b181ecb30b9507c5e0096fc
Denis Pankov <idenispankov@gmail.com>
2021-02-03 19:04:44 -0800

    alt text added to all pictures, modal responsive

--- pages/index.js
+++ pages/index.js
@@ -24,13 +24,13 @@ function closeModalOutside(e) {
 document.addEventListener('keyup', escCloseModal);
 document.addEventListener('click', closeModalOutside);
 
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-180px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// let prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   let currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-180px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
