3e9eaeb791473f9375d1c26aa73b5e732fb905a1
Denis Pankov <idenispankov@gmail.com>
2021-02-07 19:59:36 -0800

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -23,14 +23,3 @@ function closeModalOutside(e) {
 
 document.addEventListener('keyup', escCloseModal);
 document.addEventListener('click', closeModalOutside);
-
-// let prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   let currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-110px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
